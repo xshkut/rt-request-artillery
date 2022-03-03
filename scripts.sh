33f28149d3d894deb43eec7bcc78ad7cc31e2db5
cwshkut <alexey.shkut@clockwise.software>
2022-03-03 19:00:13 +0000

    simplify scripts

--- scripts.sh
+++ scripts.sh
@@ -10,20 +10,20 @@ mkdir -p docker-images/attacker
 # Compile for alpine
 docker run -v $(pwd)/docker-images/attacker:/outfile -v $(pwd):/app --workdir="/app" --env CGO_ENABLED=0 golang:latest go build -o /outfile ./cmd/attacker
 
-## In folder with binary
 # Build
-docker build --no-cache -t "tbsitg/artillery-attacker:v1.0.0" .
+docker build --no-cache -t "tbsitg/artillery-attacker:latest" ./docker-images/attacker
 # Publish
-docker push tbsitg/artillery-attacker:v1.0.0
+docker push tbsitg/artillery-attacker:latest
+# Run
+docker run -it tbsitg/artillery-attacker:latest
 
 ### COORDINATOR
 # Compile for alpine
 docker run -v $(pwd)/docker-images/coordinator:/outfile -v $(pwd):/app --workdir="/app" --env CGO_ENABLED=0 golang:latest go build -o /outfile ./cmd/coordinator
 
-## In folder with binary
 # Build
-docker build --no-cache -t "tbsitg/artillery-coordinator" .
+docker build --no-cache -t "tbsitg/artillery-coordinator:latest" ./docker-images/coordinator
 # Publish
-docker push tbsitg/artillery-coordinator
+docker push tbsitg/artillery-coordinator:latest
 # Run
-docker run -it tbsitg/artillery-coordinator
+docker run -it tbsitg/artillery-coordinator:latest
